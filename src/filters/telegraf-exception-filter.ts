791382ef82f0d71b2332d8bb14b69c1b4e8155ab
Cheri Ong <cheri.yuhann@outlook.com>
2024-01-25 14:55:52 +0800

    docs: add comments for alternate catch function

--- src/filters/telegraf-exception-filter.ts
+++ src/filters/telegraf-exception-filter.ts
@@ -7,9 +7,16 @@ export class TelegrafExceptionFilter implements ExceptionFilter {
 
   async catch(exception: TelegrafException): Promise<void> {
     this.logger.log(exception.message);
-    // const telegrafHost = TelegrafArgumentsHost.create(host);
-    // const ctx = telegrafHost.getContext<Context>();
-    //
-    // await ctx.replyWithHTML(`<b>Error</b>: ${exception.message}`);
   }
+
+  // If it is preferable to send the error message via telegram to the user, the below version of the catch function can be used instead
+  // async catch(
+  //   exception: TelegrafException,
+  //   host: ArgumentsHost,
+  // ): Promise<void> {
+  //   const telegrafHost = TelegrafArgumentsHost.create(host);
+  //   const ctx = telegrafHost.getContext<Context>();
+  //
+  //   await ctx.replyWithHTML(`<b>Error</b>: ${exception.message}`);
+  // }
 }
